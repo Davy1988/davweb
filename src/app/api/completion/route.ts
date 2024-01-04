dd6f39bef3e7c9e83dffee6fe008e2e7a44703e4
Davy1988 <dsecoosorio@gmail.com>
2024-01-03 21:12:26 -0500

    ref-Update route for process the stream response openAI in vercel edge

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,30 +1,31 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
 export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
-    const rs = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-        max_tokens: 500,
-        temperature: 0,
-        stream: true,
-        n: 1,
-      }),
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json',
-      },
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      max_tokens: 500,
+      temperature: 0,
+      stream: true,
+      n: 1,
+      messages,
     });
-    return new Response(rs.body);
-  } catch (e) {
-    return new Response(
-      JSON.stringify({
-        message: 'Please try again.Apologies for the inconvenience.',
-      }),
-      {
-        status: 500,
-      }
-    );
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      const { name, status, headers, message } = error;
+      return NextResponse.json({ name, status, headers, message }, { status });
+    } else {
+      throw error;
+    }
   }
 }
